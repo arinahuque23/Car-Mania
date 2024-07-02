a9a0ce0bb5f3b446eed641f49e56fbf196de6b45
arinahuque23 <arinahuqe23@gmail.com>
2024-07-02 14:55:14 +0600

    update

--- src/Pages/HowItWork/HowItWork.jsx
+++ src/Pages/HowItWork/HowItWork.jsx
@@ -9,10 +9,11 @@ const HowItWork = () => {
         <h1 className="text-4xl text-center mb-20">Rent your desired car with following 3 working steps</h1>
       </div>
       <div className="grid grid-cols-3 ">
-          <div className="items-center justify-center text-center">
+          <div className="items-center justify-center text-center " style={''}>
                <img className="ml-[30vh] mb-10" src="https://i.ibb.co/C7K2YrX/location.png" alt="" />
           <h1 className="text-xl font-bold">Pick Your location</h1>
           <h1>Choose your location and find your best car.</h1>
+          <h1>hjdhsdhsd</h1>
           </div>
           <div className="items-center justify-center text-center">
                <img src="https://i.ibb.co/C7K2YrX/location.png" alt="" />
